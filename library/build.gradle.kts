aae5a256bd76358bf5b7c248062c87421691f12c
Brandon Cano <brandoncano01@gmail.com>
2025-01-10 14:53:29 -0600

    1.4.4. update

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.bmcano"
-version = "1.4.3"
+version = "1.4.4"
 
 android {
     namespace = "com.brandoncano.sharedcomponents"
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         minSdk = 21
-        version = "1.4.3"
+        version = "1.4.4"
     }
 
     buildTypes {
@@ -63,7 +63,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.github.bmcano"
                 artifactId = "shared-android-components"
-                version = "1.4.3"
+                version = "1.4.4"
                 from(components.findByName("release"))
             }
         }
